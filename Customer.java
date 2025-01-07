f1989b343bba43c5459ef247a0ea61d6b2a8dd17
hritikcdy <167880564+hritikcdy@users.noreply.github.com>
2025-01-07 09:50:25 +0000

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,2 +1,22 @@
 public class Customer {
+    private String name;
+    private String customerId;
+
+    public Customer(String name, String customerId) {
+        this.name = name;
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{name='" + name + "', customerId='" + customerId + "'}";
+    }
 }
