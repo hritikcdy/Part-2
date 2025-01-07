577759edaf54bcf2dd852e64add8b10015cab56e
hritikcdy <167880564+hritikcdy@users.noreply.github.com>
2025-01-07 19:45:10 +0000

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,28 +1,14 @@
 public class Worker {
-    private ParcelMap parcelMap;
-    private QueueOfCustomers customerQueue;
-
-    public Worker(ParcelMap parcelMap, QueueOfCustomers customerQueue) {
-        this.parcelMap = parcelMap;
-        this.customerQueue = customerQueue;
-    }
-
     public double calculateFee(Parcel parcel) {
-        // Implement a basic fee calculation based on weight
-        return parcel.getWeight() * 2.5; // Fee per weight unit
+        return parcel.getWeight() * 2.5; // Example fee calculation
     }
 
-    public void processCustomer() {
-        if (!customerQueue.isEmpty()) {
-            Customer customer = customerQueue.removeCustomer();
-            Log.getInstance().logEvent("Processing customer: " + customer.getName());
-            // Simulate processing a parcel
-            Parcel parcel = parcelMap.getParcel("some_parcel_id"); // Example usage
-            if (parcel != null && !parcel.isProcessed()) {
-                double fee = calculateFee(parcel);
-                Log.getInstance().logEvent("Parcel " + parcel.getId() + " processed with fee: " + fee);
-                parcel.markAsProcessed();
-            }
-        }
+    public void processCustomer(Customer customer, Parcel parcel) {
+        double fee = calculateFee(parcel);
+        System.out.println("Processed Customer: " + customer.getName());
+        System.out.println("Parcel: " + parcel.getDetails());
+        System.out.println("Fee: £" + fee);
+        Log.getInstance().addEntry("Processed Customer: " + customer.getName() +
+                ", Parcel: " + parcel.getDetails() + ", Fee: £" + fee);
     }
 }
