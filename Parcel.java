9f6d86a3643da13192890fba91de7aa055c7fddc
hritikcdy <167880564+hritikcdy@users.noreply.github.com>
2025-01-07 09:47:48 +0000

    Update Parcel.java

--- Parcel.java
+++ Parcel.java
@@ -1,2 +1,32 @@
 public class Parcel {
+    private String id;
+    private double weight;
+    private boolean isProcessed;
+
+    public Parcel(String id, double weight) {
+        this.id = id;
+        this.weight = weight;
+        this.isProcessed = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public boolean isProcessed() {
+        return isProcessed;
+    }
+
+    public void markAsProcessed() {
+        this.isProcessed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Parcel{id='" + id + "', weight=" + weight + ", processed=" + isProcessed + "}";
+    }
 }
