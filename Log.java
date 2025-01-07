4088973b60287fe6c41c097b914268cb54c90723
hritikcdy <167880564+hritikcdy@users.noreply.github.com>
2025-01-07 09:52:05 +0000

    Update Log.java

--- Log.java
+++ Log.java
@@ -1,2 +1,31 @@
 public class Log {
+    private static Log instance;
+    private StringBuilder logContent;
+
+    private Log() {
+        logContent = new StringBuilder();
+    }
+
+    public static Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
+
+    public void logEvent(String event) {
+        logContent.append(event).append("\n");
+    }
+
+    public String getLog() {
+        return logContent.toString();
+    }
+
+    public void writeLogToFile(String filename) {
+        try {
+            java.nio.file.Files.write(java.nio.file.Paths.get(filename), logContent.toString().getBytes());
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
